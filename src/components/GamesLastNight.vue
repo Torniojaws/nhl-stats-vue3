76087f6ac58d1f135e2e75ce92bc992fa34bd78c
Juha Untinen <torniojaws@gmail.com>
2022-10-31 20:16:49 +0200

    Add spinner when loading game data

--- src/components/GamesLastNight.vue
+++ src/components/GamesLastNight.vue
@@ -7,6 +7,7 @@ import { yesterday } from '../utils/dates';
 interface StateData {
   games: any[];
   yesterday: string;
+  isLoading: boolean;
 }
 
 export default defineComponent({
@@ -16,11 +17,13 @@ export default defineComponent({
   data(): StateData {
     return {
       games: [],
-      yesterday: ''
+      yesterday: '',
+      isLoading: true,
     };
   },
   async mounted() {
     this.games = await getLastNightGamesResults();
+    this.isLoading = false;
     this.yesterday = yesterday;
   },
 });
@@ -36,7 +39,10 @@ export default defineComponent({
       :home="game.home"
     />
   </div>
-  <div v-if='games.length === 0' id='noGames'>
+  <div v-if='isLoading' id='spinnerContainer'>
+    <img :src="`spinner.gif`" id='spinner'/>
+  </div>
+  <div v-if='!isLoading && games.length === 0' id='noGames'>
     <p>No games last night ({{ yesterday }})</p>
   </div>
 </template>
@@ -48,5 +54,13 @@ div#games {
 div#noGames {
   flex: 1;
   text-align: center;
+}
+div#spinnerContainer {
+  display: flex;
+  justify-content: center;
+}
+img#spinner {
+  height: 128px;
+  width: 128px;
 }
 </style>
